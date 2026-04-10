9459480cd20ca47911497d18a42bfe279c06dff7
BellWorkspace <stevenjbell89@gmail.com>
2026-04-10 17:10:19 -0400

    Update comps to RPR agent-selected set (Medoras anchor)
    Replace 6 Paragon comps with 5 RPR comps recommended by Luke:
    - 4969 Alta Vista Ave (Closed .3M)
    - 5224 Medoras Ave (Closed .275M, anchor)
    - 5813 Rudolph Ave (Closed .2M)
    - 5810 A1a S (Pending .24M)
    - 5124 Osceola Ave (Active .169M)
    
    Updated strategy narratives and comp descriptions in both views.

--- cma-data.js
+++ cma-data.js
@@ -7,13 +7,13 @@
    dashboard, click "Save & Export Data", and replace this file
    with the downloaded copy. The client view will pick up the
    new comps the next time it's opened.
-   Last updated: 2026-04-09
+   Last updated: 2026-04-10
    ============================================================ */
 
 window.CMA_DATA = {
   meta: {
-    version: 1,
-    generated: "2026-04-09T00:00:00Z",
+    version: 2,
+    generated: "2026-04-10T00:00:00Z",
     address: "5824 Rudolph Ave",
     city: "St Augustine, FL 32080"
   },
@@ -28,51 +28,6 @@ window.CMA_DATA = {
 
   comps: [
     {
-      addr: "5730 Rudolph Ave",
-      subdivision: "Butlers Beach \u00b7 Area 07",
-      city: "Saint Augustine FL 32080",
-      status: "Pending",
-      price: 1495000,
-      sqft: 2016,
-      beds: 4,
-      baths: 4,
-      dom: 15,
-      monthsAgo: 0.5,
-      mls: "261400",
-      photo: "https://zimg.paragon.ice.com/ParagonImages/Property/PL/BORMLS/261400/0/120/90/eec13d6e0227146f6602246379567ab8/1/a7e89d7bd454780770c48a9284450623/261400-7B81E3B8-D64D-4B2A-BD1C-BFFDB858C902.JPG",
-      anchor: true
-    },
-    {
-      addr: "6525 Broward St",
-      subdivision: "Ocean Grove \u00b7 Area 07",
-      city: "Saint Augustine FL 32080",
-      status: "Closed",
-      price: 1572500,
-      sqft: 2763,
-      beds: 4,
-      baths: 4,
-      dom: 8,
-      closingDate: "2/9/2026",
-      monthsAgo: 2,
-      mls: "260033",
-      photo: "https://zimg.paragon.ice.com/ParagonImages/Property/PL/BORMLS/260033/0/120/90/5255b824a42649b63eae4d78801abab0/1/25a078a645262f6646c0f2f6cde03a2e/260033-2D84DA23-0D29-49EF-B35E-8F613BC9BB5F.JPG"
-    },
-    {
-      addr: "110 7th Street",
-      subdivision: "Chatauqua Beach \u00b7 Area 07",
-      city: "Saint Augustine Beach FL 32080",
-      status: "Closed",
-      price: 1600000,
-      sqft: 3246,
-      beds: 5,
-      baths: 6,
-      dom: 57,
-      closingDate: "3/23/2026",
-      monthsAgo: 0.5,
-      mls: "260293",
-      photo: "https://zimg.paragon.ice.com/ParagonImages/Property/PL/BORMLS/260293/0/120/90/216dd3af377af9e88206aed9a0b69a49/1/a81fdb8a0793ef2b5fab091a10b3c699/260293-b4e3dbcb-3b91-4239-94b8-7fc08edeac96.JPG"
-    },
-    {
       addr: "4969 Alta Vista Ave",
       subdivision: "Biera Mar \u00b7 Area 07",
       city: "Saint Augustine FL 32080",
@@ -85,35 +40,66 @@ window.CMA_DATA = {
       closingDate: "1/23/2026",
       monthsAgo: 2.5,
       mls: "256056",
-      photo: "https://zimg.paragon.ice.com/ParagonImages/Property/PL/BORMLS/256056/0/120/90/714a28694ca00b31a6effeca65c9319f/1/9072c45e41cb8df21e30ec3e0aaa99b4/256056-5b0b7f77-80d7-4fd9-80c2-22272a001802.JPG"
+      photo: ""
     },
     {
-      addr: "5599 Pelican Way",
-      subdivision: "Anastasia Hills \u00b7 Area 07",
+      addr: "5224 Medoras Ave",
+      subdivision: "Butler Beach \u00b7 Area 07",
       city: "Saint Augustine FL 32080",
-      status: "Active",
-      price: 1698000,
-      sqft: 2539,
-      beds: 4,
-      baths: 3.5,
-      dom: 68,
-      monthsAgo: 0,
-      mls: "260663",
-      photo: "https://zimg.paragon.ice.com/ParagonImages/Property/PL/BORMLS/260663/0/120/90/d3b2ae4cda85cf936272f604259db014/1/42683c8ac23d5cff934a61a455ac5b20/260663-a651c5c4-0029-4f11-9bdd-9ef528ed1686.JPG"
+      status: "Closed",
+      price: 1275000,
+      sqft: 2353,
+      beds: 3,
+      baths: 2.5,
+      dom: 70,
+      closingDate: "10/28/2025",
+      monthsAgo: 5.4,
+      mls: "254214",
+      photo: "",
+      anchor: true
     },
     {
-      addr: "9106 Mellon Ct",
-      subdivision: "Matanzas Inlet Bch \u00b7 Area 07",
+      addr: "5813 Rudolph Ave",
+      subdivision: "Butler Beach \u00b7 Area 07",
+      city: "Saint Augustine FL 32080",
+      status: "Closed",
+      price: 1200000,
+      sqft: 2680,
+      beds: 3,
+      baths: 2.5,
+      dom: 228,
+      closingDate: "11/6/2025",
+      monthsAgo: 5.1,
+      mls: "250778",
+      photo: ""
+    },
+    {
+      addr: "5810 A1a S",
+      subdivision: "Butler Beach \u00b7 Area 07",
+      city: "Saint Augustine FL 32080",
+      status: "Pending",
+      price: 1239800,
+      sqft: 3178,
+      beds: 5,
+      baths: 4,
+      dom: 212,
+      monthsAgo: 0,
+      mls: "2108325",
+      photo: ""
+    },
+    {
+      addr: "5124 Osceola Ave",
+      subdivision: "Butler Beach \u00b7 Area 07",
       city: "Saint Augustine FL 32080",
       status: "Active",
-      price: 1100000,
-      sqft: 2580,
+      price: 1169000,
+      sqft: 3001,
       beds: 4,
       baths: 3.5,
-      dom: 23,
+      dom: 67,
       monthsAgo: 0,
-      mls: "261257",
-      photo: "https://zimg.paragon.ice.com/ParagonImages/Property/PL/BORMLS/261257/0/120/90/fd2df6500925594424d9e26b3213efc3/1/bcfabff2d8ae0d076267c3263c507791/261257-7eca7d02-3b19-4b9c-bd5a-6ed7ea0d5499.JPG"
+      mls: "260573",
+      photo: ""
     }
   ]
 };
